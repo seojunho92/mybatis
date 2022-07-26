b0d7f13d3234a6bc35e9ada0ad0b5482aba52658
a <a@a.com>
2022-07-26 17:38:48 +0900

    '07-26'

---
+++ src/ch01/User.java
@@ -0,0 +1,38 @@
+package ch01;
+
+import java.sql.Date;
+
+public class User {
+	private int userId;
+	private String userName;
+	private Date regDate;
+	
+	public int getUserId() {
+		return userId;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public Date getRegDate() {
+		return regDate;
+	}
+	
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%d %s %s", userId, userName, regDate);
+	}
+}
---
+++ src/ch02/ex03/Main.java
@@ -0,0 +1,32 @@
+package ch02.ex03;
+
+import java.time.LocalDate;
+
+import ch02.domain.User;
+import config.Configuration;
+
+public class Main {
+	public static void main(String[] args) {
+		Map mapper = Configuration.getMapper(Map.class);
+		
+		
+		if(mapper.insertUser(new User(11, "abel", LocalDate.now())) > 0)
+			System.out.println("己傍 11");
+		
+		
+		
+		if(mapper.insertUser(new User(12, null, null)) > 0)
+			System.out.println("己傍 12");
+	
+		
+	
+		if(mapper.insertUser2(13, "kianu", LocalDate.of(2022, 8, 15)) > 0)
+			System.out.println("己傍 13");
+	
+		
+		
+		if(mapper.insertUser3(14, "neo", LocalDate.now()) > 0)
+			System.out.println("己傍 14");
+			
+	}
+}
